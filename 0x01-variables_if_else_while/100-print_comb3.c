dd600e08851fe99ab8a657c723e1d74f247b2baa
Ahmed Mohamed <dolanegy@.com>
2023-02-17 21:42:17 +0200

    Added file 100-print_comb3.c to solve task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *main - this will print the smallest different combination of two digits
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (i < j && i != j)
+			{
+				putchar(i);
+				putchar(j);
+			
+				if ((i + j) != ('8' + '9'))
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
