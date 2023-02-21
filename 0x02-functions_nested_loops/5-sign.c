2f4c42177c7349ca8ab3799984d4d079b66c7bbd
Ahmed Mohamed <dolanegy@.com>
2023-02-21 17:31:49 +0200

    Added files 5-sign.c and 5-main.c to solve task 5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - this will print the sign of n
+ *
+ * @n: the parameter to be printed
+ *
+ *Return: 1 if n is positive 0 if n is zero and -1 if n is negative
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
