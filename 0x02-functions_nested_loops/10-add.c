f796534ef72c62f145dfe5614bfc2571cb29a98b
Ahmed Mohamed <dolanegy@.com>
2023-02-21 20:52:41 +0200

    Added files 10-add.c and 10-main.c to solve task 10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * @num1: parameter for the first number
+ * @num2: parameter for the secon number
+ *
+ *Return: the result of adding two integers
+ */
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+
+	return (sum);
+}
