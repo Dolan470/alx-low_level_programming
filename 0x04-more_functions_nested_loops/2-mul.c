5cdf17f5cfc16c28a4a6207749a17f2f8a67a3e6
Ahmed Mohamed <dolanegy@.com>
2023-02-23 14:35:40 +0200

    Added files 2-mul.c and 2-main.c to solve task 2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first parameter to be multiplied
+ * @b: second parameter to be multiplied
+ * Return: the result
+ */
+int mul(int a, int b)
+{
+	int multi = a * b;
+
+	return (multi);
+}
