51128a3ca4f131cd6f5aac04f9cbbc4d76341cd4
Ahmed Mohamed <dolanegy@.com>
2023-02-28 00:27:39 +0200

    Added files 8-print_array.c and 8-main.c to solve task: 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - prints n elements of an array of integers.
+ * @a: a parameter for an array of integers
+ * @n: a parameter for the number of elements of the array to be printed
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%i ", a[i]);
+		i++;
+	}
+	printf("\n");
+}
