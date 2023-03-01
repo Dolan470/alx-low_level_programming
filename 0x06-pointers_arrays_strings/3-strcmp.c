78dc493358244d60e0d4addfc96954907c2ec238
Ahmed Mohamed <dolanegy@.com>
2023-03-01 19:38:41 +0200

    Changed files 3-strcmp.c to solve task: 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,20 +11,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, k;
+	char *p1 = s1, *p2 = s2;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	while (*p1 != '\0' && *p2 != '\0' && *p1 == *p2)
 	{
-		if (s1[i] == s2[i])
-		{
-			return (0);
-		}
-		else
-		{
-			k = s1[i] - s2[i];
-			return (k);
-		}
-
+		p1++;
+		p2++;
 	}
-	return (0);
+	return (*p1 - *p2);
 }
